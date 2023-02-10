9d35f8db46188e8935d4075df478854d1fa30066
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-02-10 10:41:54 -0300

    Card component + style changes

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div>
+  <main>
     <form @submit.prevent="fetchPokemon">
       <input type="text" v-model="pokemonName" />
       <button type="submit">Submit</button>
     </form>
 
     <div v-if="stats">
-      <img :src="pokemonImage" alt="" />
-      <ul>
-        <li v-for="stat in stats" :key="stat.stat.name">
-          {{ stat.stat.name }}: {{ stat.base_stat }}
-        </li>
-      </ul>
+      <Card :name="pokemonName" :stats="this.stats" :imageSrc="pokemonImage" />
     </div>
-    <div v-if="evolutionChain">
-      Evolutions:
-      <ul></ul>
-    </div>
-  </div>
+    <div v-if="evolutionChain">Evolutions:</div>
+  </main>
 </template>
 
 <script>
 import axios from "axios";
+import Card from "./components/Card.vue";
 
 export default {
   data() {
@@ -32,6 +25,7 @@ export default {
       pokemonImage: "",
     };
   },
+  components: { Card },
   methods: {
     async fetchPokemon() {
       try {
@@ -41,10 +35,14 @@ export default {
         this.stats = response.data.stats;
         this.pokemonImage = response.data.sprites.front_default;
         const speciesUrl = response.data.species.url;
+
         const speciesResponse = await axios.get(speciesUrl);
         const evolutionUrl = speciesResponse.data.evolution_chain.url;
+
         const evolutionChain = await axios.get(evolutionUrl);
         this.evolutionChain = evolutionChain.data.chain;
+
+        console.log(this.evolutionChain);
       } catch (error) {
         console.error(error);
         this.evolutionChain = null;
---
+++ src/components/Card.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card" @click="displayBlock = !displayBlock">
+    <div class="card-header">
+      {{ name }}
+    </div>
+    <img :src="imageSrc" />
+    <div class="card-body" v-show="displayBlock">
+      <ul>
+        <li v-for="stat in stats" :key="stat.stat.name">
+          {{ stat.stat.name }}: {{ stat.base_stat }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template> 
+
+<script>
+export default {
+  methods: {
+    displayStats(element) {
+      return (element.display = "block");
+    },
+  },
+  props: {
+    stats: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      displayBlock: false,
+    };
+  },
+};
+</script>
+
+<style>
+.card {
+  min-width: 200px;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  margin: 20px;
+  transition: 0.25s;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  transform: translateY(-5px);
+}
+.card-header {
+  background-color: lightblue;
+  color: white;
+  padding: 10px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  width: 100%;
+}
+
+.card-body {
+  padding: 10px;
+}
+
+.card-body ul {
+  list-style-type: none;
+}
+</style>
