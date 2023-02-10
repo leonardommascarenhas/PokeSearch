d2a6cf1a573b5877f0a121607df3a6f049b6b3bd
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-02-10 16:39:00 -0300

    No display of pokemons that doesn't evolves

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <form @submit.prevent="fetchPokemon">
+    <form @submit.prevent="searchPokemon">
       <input type="text" v-model="pokemon.name" />
       <button type="submit">Submit</button>
     </form>
@@ -36,31 +36,48 @@ export default {
   },
   components: { Card },
   methods: {
-    async fetchPokemon() {
+    async fetchPokemon(pokemon) {
       try {
         const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${this.pokemon.name.toLowerCase()}`
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name
+            .replace("'", "")
+            .replace(/[.: ']+/g, "-")
+            .toLowerCase()}`
         );
-        this.pokemon.stats = response.data.stats;
-        this.pokemon.pokemonImage = response.data.sprites.front_default;
+        pokemon.stats = response.data.stats;
+        pokemon.pokemonImage = response.data.sprites.front_default;
         const speciesUrl = response.data.species.url;
 
         const speciesResponse = await axios.get(speciesUrl);
         const evolutionUrl = speciesResponse.data.evolution_chain.url;
 
         const evolutionData = await axios.get(evolutionUrl);
-        this.pokemon.evolutionData = evolutionData.data.chain.evolves_to[0];
-        this.addPokemon(this.pokemon);
-        console.log(this.pokemon);
+        pokemon.evolutionData = evolutionData.data.chain.evolves_to[0];
+        this.addPokemon(pokemon);
       } catch (error) {
         console.error(error);
         this.pokemon.evolutionData = null;
       }
     },
+    async searchPokemon() {
+      try {
+        this.fetchPokemon(this.pokemon);
+      } catch (error) {
+        console.error(error);
+        this.pokemon.evolutionData = null;
+      }
+    },
+    async hasEvolution(pokemon) {
+      try {
+        if (pokemon.evolutionData.evolves_to !== []) {
+          console.group(pokemon);
+        }
+      } catch {}
+    },
     addPokemon(pokemon) {
+      this.hasEvolution(this.pokemon);
       this.pokemonArray.push(pokemon);
       this.pokemon = {};
-      console.log(this.pokemon);
     },
   },
 };
